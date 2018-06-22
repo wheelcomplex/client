6195e8d66003b4ecce4553e3d52e66921d4976d3
Joshua Blum <jblum18@gmail.com>
2018-06-22 14:48:50 -0400

    Extend ek staleness window to one month

--- go/ephemeral/lib.go
+++ go/ephemeral/lib.go
@@ -204,8 +204,8 @@ func (e *EKLib) newDeviceEKNeeded(ctx context.Context, merkleRoot libkb.MerkleRo
 	}
 
 	// Ok we can access the ek, check lifetime.
-	e.G().Log.CDebugf(ctx, "nextDeviceEKNeeded at: %v", nextKeygenTime(ek.Metadata.Ctime))
-	return keygenNeeded(ek.Metadata.Ctime, merkleRoot), nil
+	e.G().Log.CDebugf(ctx, "nextDeviceEKNeeded at: %v", nextKeygenTime(ek.Metadata.Ctime.Time()))
+	return keygenNeeded(ek.Metadata.Ctime.Time(), merkleRoot), nil
 }
 
 func (e *EKLib) NewUserEKNeeded(ctx context.Context) (needed bool, err error) {
@@ -247,8 +247,8 @@ func (e *EKLib) newUserEKNeeded(ctx context.Context, merkleRoot libkb.MerkleRoot
 		}
 	}
 	// Ok we can access the ek, check lifetime.
-	e.G().Log.CDebugf(ctx, "nextUserEKNeeded at: %v", nextKeygenTime(ek.Metadata.Ctime))
-	return keygenNeeded(ek.Metadata.Ctime, merkleRoot), nil
+	e.G().Log.CDebugf(ctx, "nextUserEKNeeded at: %v", nextKeygenTime(ek.Metadata.Ctime.Time()))
+	return keygenNeeded(ek.Metadata.Ctime.Time(), merkleRoot), nil
 }
 
 func (e *EKLib) NewTeamEKNeeded(ctx context.Context, teamID keybase1.TeamID) (needed bool, err error) {
@@ -294,8 +294,8 @@ func (e *EKLib) newTeamEKNeeded(ctx context.Context, teamID keybase1.TeamID, mer
 		}
 	}
 	// Ok we can access the ek, check lifetime.
-	e.G().Log.CDebugf(ctx, "nextTeamEKNeeded at: %v", nextKeygenTime(ek.Metadata.Ctime))
-	return keygenNeeded(ek.Metadata.Ctime, merkleRoot), latestGeneration, nil
+	e.G().Log.CDebugf(ctx, "nextTeamEKNeeded at: %v", nextKeygenTime(ek.Metadata.Ctime.Time()))
+	return keygenNeeded(ek.Metadata.Ctime.Time(), merkleRoot), latestGeneration, nil
 }
 
 type teamEKGenCacheEntry struct {
